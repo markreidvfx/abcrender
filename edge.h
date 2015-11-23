4567cf34c9aed9bae4ea8ad2b09769ec4966c3c5
Mark Reid <mindmark@gmail.com>
2015-11-22 17:23:33 -0800

    removed unused funcs and vars

--- edge.h
+++ edge.h
@@ -17,8 +17,7 @@ public:
     int yend() const {return m_yend;}
     float x() const {return m_x;}
     glm::vec3 color() const {return m_color;}
-    float one_overz() const {return m_one_overz;}
-    glm::vec2 uv() const {return m_uv;}
+
 private:
     float m_x;
     float m_xstep;
@@ -27,12 +26,6 @@ private:
 
     glm::vec3 m_color;
     glm::vec3 m_colorstep;
-
-    float m_one_overz;
-    float m_one_overz_step;
-
-    glm::vec2 m_uv;
-    glm::vec2 m_uvstep;
 
 };
 
